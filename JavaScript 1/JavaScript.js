d070b653b83bfe18570e981814e71daba47e30f2
Xx-Darkness-xX02 <kjell.roelofs@student.rocvf.nl>
2018-10-03 09:30:56 +0200

    burton

---
+++ JavaScript 1/JavaScript.js
@@ -0,0 +1,15 @@
+var mijnauto = {
+    merk:"ford",
+    type: "mondeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+
+}
+
+console.log(mijnauto.kleur)
+
+for(var i = 0; i < 10; i++) {
+    console.log("i heeft de waarde " +i);
+}
+
+console.log("hallo wereld")
