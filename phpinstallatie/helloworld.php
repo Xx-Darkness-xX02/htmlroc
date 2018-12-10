e0b1481ad60b3d6458233eb028715c92c1b93226
Xx-Darkness-xX02 <kjell.roelofs@student.rocvf.nl>
2018-12-10 09:33:48 +0100

    burton

--- phpinstallatie/helloworld.php
+++ phpinstallatie/helloworld.php
@@ -5,4 +5,34 @@
  * Date: 11/23/2018
  * Time: 8:41 AM
  */
-echo "hello world";
+$a = "ik heb niet gefaald";
+
+$b = " die niet werken";
+
+$c = " 10000";
+
+echo $a," ik heb alleen".$c," manieren".$b, ".";
+
+$d = " die geen fout";
+
+$e = "en";
+
+$f = "niet";
+
+echo "Mens".$e.$d.$e." maken"." werk".$e.$f;
+
+$g = "ry";
+
+$h = ", but";
+
+$i = "fail";
+
+echo "T".$g." and ".$i.$h." never ".$i." to t".$g;
+
+$j = "\"omgaan";
+
+$k = "met";
+
+$l = "teleu";
+
+echo "De cursus ".$j.$k.$l."rstellingen"."\""." kan vanavond helaas niet doorgaan";
